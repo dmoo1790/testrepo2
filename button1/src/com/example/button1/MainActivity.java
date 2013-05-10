a78daf24a523b7a5ed07de400b5c912548f2c7ed
David Moore <dmoo1790@ihug.co.nz>
2013-05-10 17:14:04 +1200

    Revert "Revert "another test""
    This reverts commit fbabdf53213b3c0dd11554b3daabb3a8c0824043.

--- button1/src/com/example/button1/MainActivity.java
+++ button1/src/com/example/button1/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity implements OnClickListener
 //		setContentView(btn, null);
 		btn = (Button) findViewById(R.id.button1);
 		btn.setOnClickListener(this);
+		// Some stuff to commit
 	}
 
 	@Override
